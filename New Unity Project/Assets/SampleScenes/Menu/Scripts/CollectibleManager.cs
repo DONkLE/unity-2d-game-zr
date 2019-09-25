8706027d4d7f47e3b7053fe2e1615564b9a6734c
Zach Ridings <zridings@gmail.com>
2019-09-25 15:29:56 -0500

    Transfer project to git

---
+++ New Unity Project/Assets/SampleScenes/Menu/Scripts/CollectibleManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CollectibleManager : MonoBehaviour
+{
+    public static CollectibleManager Instance;
+
+    private int _totalBurgs;
+    private int _collectedBurgs;
+
+    public TextMeshProUGUI burgCount;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    private void Start()
+    {
+        _totalBurgs = FindObjectsOfType<Collectible>().Length;
+    }
+
+    void DisplayBurgCount()
+    {
+        burgCount.SetText(_collectedBurgs + " / " + _totalBurgs);
+
+
+    }
+
+   public void HandleBurgCollected()
+    {
+        _collectedBurgs++;
+        DisplayBurgCount();
+    }
+}
---
+++ New Unity Project/Assets/Scripts/Collectible.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        HandleCollected();
+    }
+
+        void HandleCollected()
+    {
+        CollectibleManager.Instance.HandleBurgCollected();
+        print("Picked up");
+        Destroy(gameObject);
+
+    }
+}
